18f0f29bc0bc77d182b89579a800dffd90a108aa
MrYsLab <MisterYsLab@gmail.com>
2025-12-11 17:19:07 -0500

    i2c examples

--- examples/WIFI/threaded/i2c_adxl345_accelerometer_wt.py
+++ examples/WIFI/threaded/i2c_adxl345_accelerometer_wt.py
@@ -17,11 +17,12 @@
 
 import sys
 import time
-from telemetrix_rpi_pico_2w_serial import telemetrix_rpi_pico_2w_serial
+from telemetrix_rpi_pico_2w_wifi import telemetrix_rpi_pico_2w_wifi
 
 """
 This example sets up and control an ADXL345 i2c accelerometer for i2c port 0.
 It will continuously print data the raw xyz data from the device.
+
 SDA = pin 4   SCL = pin 5
 """
 
@@ -39,7 +40,7 @@ def the_callback(data):
     date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_stamp))
     print(f'Raw Data:  {data}')
     print(f'ADXL345 Report On: {date}: ')
-    print(f'\t\ti2c_port={data[1]} x-pair={data[5]}, '
+    print(f'\t\ti2c_port={ data[1]} x-pair={data[5]}, '
           f'{data[6]}  y-pair={data[7]}, '
           f'{data[8]} z-pair={data[9]}, '
           f'{data[10]}')
@@ -50,32 +51,32 @@ def adxl345(my_board):
     # setup adxl345
     # device address = 83
     my_board.set_pin_mode_i2c(0)
-    time.sleep(.2)
+    # time.sleep(.2)
 
     # set up power and control register
     my_board.i2c_write(83, [45, 0], i2c_port=0)
     # time.sleep(.2)
     my_board.i2c_write(83, [45, 8], i2c_port=0)
-    time.sleep(.2)
+    # time.sleep(.2)
 
     # set up the data format register
     my_board.i2c_write(83, [49, 8], i2c_port=0)
-    time.sleep(.5)
+    # time.sleep(.5)
     my_board.i2c_write(83, [49, 3], i2c_port=0)
-    time.sleep(.1)
+    # time.sleep(.1)
 
     # read_count = 20
     while True:
         # read 6 bytes from the data register
         try:
             my_board.i2c_read(83, 50, 6, i2c_port=0,  callback=the_callback)
-            time.sleep(.3)
+            # time.sleep(.3)
         except (KeyboardInterrupt, RuntimeError):
             my_board.shutdown()
             sys.exit(0)
 
 
-board = telemetrix_rpi_pico_2w_serial.TelemetrixRpiPico2wSerial()
+board = telemetrix_rpi_pico_2w_wifi.TelemetrixRpiPico2WiFi(ip_address='192.168.2.212')
 try:
     adxl345(board)
 except KeyboardInterrupt:
